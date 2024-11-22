fb73a37bfccbf90e6c60f9c51e268fcd52de56c1
MarcBotana <marc.botana@gmail.com>
2024-11-22 09:35:41 +0100

    Creació Model Reserva, edició relacions OneToOne i Identificador classes.

--- src/main/java/model/Vehicle.java
+++ src/main/java/model/Vehicle.java
@@ -9,7 +9,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
