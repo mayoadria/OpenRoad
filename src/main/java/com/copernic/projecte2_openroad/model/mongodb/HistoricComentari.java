fb607d35b7e4d12f50ce27dc89283b7c145cf73d
MarcBotana <marc.botana@gmail.com>
2024-11-25 12:42:57 +0100

    Creació Models MongoDB

--- src/main/java/com/copernic/projecte2_openroad/model/mongodb/HistoricComentari.java
+++ src/main/java/com/copernic/projecte2_openroad/model/mongodb/HistoricComentari.java
@@ -1,6 +1,29 @@
 package com.copernic.projecte2_openroad.model.mongodb;
 
+import java.time.LocalDate;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.copernic.projecte2_openroad.model.mysql.Client;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class HistoricComentari {
 
+    @Id
+    private Long idComentari;
+
+    private Client client;
+    private LocalDate dataComentari;
+    private Float valoracio;
+    private Boolean recomanacio;
+    private String missatgeComentari;
+    private int like;
 }
--- src/main/java/com/copernic/projecte2_openroad/model/mongodb/HistoricDocumentacio.java
+++ src/main/java/com/copernic/projecte2_openroad/model/mongodb/HistoricDocumentacio.java
@@ -1,5 +1,22 @@
 package com.copernic.projecte2_openroad.model.mongodb;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class HistoricDocumentacio {
 
+    @Id
+    private Long idDocument;
+
+    private String tipusDocument;
+
+    private String documentLink;
 }
