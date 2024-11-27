55efb79d23f0832bfa167cb408eb21e64c7256c4
AdriaMayo <mayo.ortiz.adria@alumnat.copernic.cat>
2024-11-27 09:53:30 +0100

    Funcionalitat de fer un registre d'un client

--- src/main/java/com/copernic/projecte2_openroad/controllers/RegistroClienteController.java
+++ src/main/java/com/copernic/projecte2_openroad/controllers/RegistroClienteController.java
@@ -1,9 +1,14 @@
 package com.copernic.projecte2_openroad.controllers;
 
 import com.copernic.projecte2_openroad.model.enums.Reputacio;
+import com.copernic.projecte2_openroad.model.mysql.Client;
+import com.copernic.projecte2_openroad.service.mysql.ClientServiceSQL;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -11,15 +16,23 @@ import java.util.List;
 @Controller
 @RequestMapping("/registre")
 public class RegistroClienteController {
+
+    @Autowired
+    ClientServiceSQL clientServiceSQL;
+
     @GetMapping("")
     public String Registre(Model model) {
         // Pasar los valores del enum al modelo
         model.addAttribute("estados", Reputacio.values());
         return "Registre"; // Nombre del archivo HTML de la vista
     }
-//
-//    @GetMapping("/")
-//    public String findAll(Model model){
-//        return "MenuInicial";
-//    }
+
+    @PostMapping("/new")
+    public String save(@ModelAttribute("client") Client cli){
+
+        clientServiceSQL.guardarClient(cli);
+
+        return "redirect:/login";
+
+    }
 }
