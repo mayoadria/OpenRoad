665ff617e125d1f770e8c53c0fed09eb0fe59bc8
AdriaMayo <mayo.ortiz.adria@alumnat.copernic.cat>
2024-11-28 13:52:07 +0100

    Afegir un boto al inici de sesio per poder registrar-se

--- src/main/java/com/copernic/projecte2_openroad/controllers/MenuPrincipalController.java
+++ src/main/java/com/copernic/projecte2_openroad/controllers/MenuPrincipalController.java
@@ -5,12 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//@RequestMapping("/")
 public class MenuPrincipalController {
 
-//    @GetMapping("/inici")
-//        public String index(){
-//            return "index";
-//        }
+    @GetMapping("/")
+        public String index(){
+            return "index";
+        }
 
 }
