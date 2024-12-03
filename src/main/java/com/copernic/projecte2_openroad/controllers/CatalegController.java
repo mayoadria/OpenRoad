bd97efe9cf49e12277aceec70a1093b6a1547306
MarcBotana <marc.botana@gmail.com>
2024-12-03 10:40:27 +0100

    Controllers Logout Inici i Catàleg

--- src/main/java/com/copernic/projecte2_openroad/controllers/CatalegController.java
+++ src/main/java/com/copernic/projecte2_openroad/controllers/CatalegController.java
@@ -2,9 +2,14 @@ package com.copernic.projecte2_openroad.controllers;
 
 import com.copernic.projecte2_openroad.model.mysql.Vehicle;
 import com.copernic.projecte2_openroad.service.mysql.VehicleServiceSQL;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -48,5 +53,14 @@ public class CatalegController {
     public String crearVehiculo(@ModelAttribute Vehicle vehicle) {
         vehicleServiceSQL.guardarVehicle(vehicle);
         return "redirect:/cataleg";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/";
     }
 }
--- src/main/java/com/copernic/projecte2_openroad/controllers/MenuPrincipalController.java
+++ src/main/java/com/copernic/projecte2_openroad/controllers/MenuPrincipalController.java
@@ -2,10 +2,13 @@ package com.copernic.projecte2_openroad.controllers;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @Controller
@@ -27,6 +30,14 @@ public class MenuPrincipalController {
         return "index";
     }
     
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/";
+    }
 
     @GetMapping("/menuVehicles")
     public String menuVehicles() {
