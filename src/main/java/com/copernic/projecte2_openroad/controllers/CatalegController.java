0852ca37a27744568d12e539ab2a09ec0010928b
MarcBotana <marc.botana@gmail.com>
2024-12-10 10:09:01 +0100

    Redirecció de catàleg a vehicle

--- src/main/java/com/copernic/projecte2_openroad/controllers/CatalegController.java
+++ src/main/java/com/copernic/projecte2_openroad/controllers/CatalegController.java
@@ -10,9 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class CatalegController {
@@ -50,4 +53,13 @@ public class CatalegController {
         vehicleServiceSQL.guardarVehicle(vehicle);
         return "redirect:/cataleg";
     }
+
+    @GetMapping("/{matricula}")
+    public String detallsVehicle(@PathVariable("matricula") String matricula, Model model) {
+        Vehicle vehicle = vehicleServiceSQL.findByMatricula(matricula).get();
+
+        model.addAttribute("vehicle", vehicle);
+        return "infoVehiculo";
+    }
+    
 }
