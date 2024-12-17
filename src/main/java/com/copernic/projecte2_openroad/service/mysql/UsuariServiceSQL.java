7864e8fe705d9883d016be1898bf699a6eb160bf
AdriaMayo <mayo.ortiz.adria@alumnat.copernic.cat>
2024-12-17 14:19:04 +0100

    borrar el crear coche automatico

--- src/main/java/com/copernic/projecte2_openroad/service/mysql/UsuariServiceSQL.java
+++ src/main/java/com/copernic/projecte2_openroad/service/mysql/UsuariServiceSQL.java
@@ -41,11 +41,6 @@ public class UsuariServiceSQL {
         return user; // Devuelve null si no se encuentra en ningún repositorio
     }
 
-    public void activateUser(String nomUsuari) {
-        Client user = clientRepository.findByNomUsuari(nomUsuari);
-        user.setEnabled(true);
-        clientRepository.save(user);
-    }
 
     /**
      * Guarda un cliente en la base de datos.
