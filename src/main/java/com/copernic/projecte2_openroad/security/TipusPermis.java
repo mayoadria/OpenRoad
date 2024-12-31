efba0158c5e784a6baa9ba7d03fe7e2e31fa7d9d
mayoa <mayo.ortiz.adria@alumnat.copernic.cat>
2024-12-31 18:44:34 +0100

    Acabar los permisos usuario para que cada tipo de usuario pueda entrar a cada pagina que tiene permitida

--- src/main/java/com/copernic/projecte2_openroad/security/TipusPermis.java
+++ src/main/java/com/copernic/projecte2_openroad/security/TipusPermis.java
@@ -1,11 +1,8 @@
 package com.copernic.projecte2_openroad.security;
 
 public enum TipusPermis {
-    MOSTRAR_DASHBOARDADMIN,
-    MOSTRAR_DASHBOARDAGENT,
-    CREAR_VEHICLE,
-    CONSULTAR_CATALEG,
-    MODIFICAR_PERFIL,
-
+ADMIN,
+    AGENT,
+    CLIENT
 
 }
