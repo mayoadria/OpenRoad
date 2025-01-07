10a03ac0b646251e2492a43b372d87e411f72af7
mayoa <mayo.ortiz.adria@alumnat.copernic.cat>
2025-01-07 13:59:58 +0100

    traduccion al catalan mas los javadocs

--- src/main/java/com/copernic/projecte2_openroad/controllers/AgentDashboardController.java
+++ src/main/java/com/copernic/projecte2_openroad/controllers/AgentDashboardController.java
@@ -23,11 +23,15 @@ import com.copernic.projecte2_openroad.service.mysql.ReservaServiceSQL;
 import com.copernic.projecte2_openroad.service.mysql.VehicleServiceSQL;
 import org.springframework.web.multipart.MultipartFile;
 
+/**
+ * Controlador per al panell de l'agent.
+ * Gestiona la visualització, creació, modificació i eliminació de vehicles, reserves i incidències.
+ */
 @Controller
 @RequestMapping("/agent")
 public class AgentDashboardController {
 
-    // MySQL
+    // Serveis de MySQL
     @Autowired
     VehicleServiceSQL vehicleServiceSQL;
 
@@ -37,16 +41,24 @@ public class AgentDashboardController {
     @Autowired
     IncidenciaServiceSQL incidenciaServiceSQL;
 
-
-
-    // MongoDB
-
+    // Serveis de MongoDB
     @Autowired
     ReservaServiceMongo reservaServiceMongo;
 
     @Autowired
     IncidenciaServiceMongo incidenciaServiceMongo;
 
+    /**
+     * Mètode que mostra el panell d'agent amb la informació dels vehicles, reserves i incidències.
+     * Permet filtrar vehicles per matrícula, estat, marca i model.
+     *
+     * @param matriculaFilt Filtres de matrícula dels vehicles.
+     * @param estatsFilt Filtres d'estat dels vehicles.
+     * @param marquesFilt Filtres de marca dels vehicles.
+     * @param modelsFilt Filtres de model dels vehicles.
+     * @param model Model de l'objecte per enviar a la vista.
+     * @return Vista del panell de l'agent.
+     */
     @GetMapping("/dashboard")
     public String dashboardAgent(
             @RequestParam(name = "matricula", required = false) String matriculaFilt,
@@ -55,15 +67,14 @@ public class AgentDashboardController {
             @RequestParam(name = "models", required = false) String modelsFilt,
             Model model) {
 
-
         Object agentObj = UserUtils.obtenirDadesUsuariModel(model);
         if (!(agentObj instanceof Agent)) {
-            model.addAttribute("error", "No se pudo obtener los datos del agente.");
-            return "errorPage"; // Cambiar a una página de error adecuada
+            model.addAttribute("error", "No s'han pogut obtenir les dades de l'agent.");
+            return "errorPage"; // Canviar a una pàgina d'error adequada
         }
         Agent agent = (Agent) agentObj;
 
-        // Obtener vehículos en la misma localidad que el agente
+        // Obtenir vehicles a la mateixa localitat que l'agent
         List<Vehicle> vehicles = vehicleServiceSQL.getVehiclesByAgentLocalitat(agent.getLocalitat().getCodiPostalLoc());
         List<Reserva> reserves = reservaServiceSQL.llistarReserves();
         List<Incidencia> incidencies = incidenciaServiceSQL.llistarIncidencies();
@@ -76,6 +87,7 @@ public class AgentDashboardController {
         List<String> models = vehicleServiceSQL.getAtributsVehicle(Vehicle::getModel, vehicles).stream()
                 .map(String::toLowerCase).collect(Collectors.toList());
 
+        // Aplicar els filtres
         if (matriculaFilt != null && !matriculaFilt.isEmpty()) {
             vehicles = vehicles.stream()
                     .filter(v -> v.getMatricula().equalsIgnoreCase(matriculaFilt))
@@ -108,8 +120,16 @@ public class AgentDashboardController {
         return "dashboardAgent";
     }
 
+    /**
+     * Mètode que mostra els detalls d'un vehicle específic.
+     *
+     * @param matricula Matricula del vehicle a mostrar.
+     * @param visualizar Valor que indica si el vehicle es visualitzarà o es podrà editar.
+     * @param model Model de l'objecte per enviar a la vista.
+     * @return Vista per modificar un vehicle.
+     */
     @GetMapping("/vehicle/{matricula}")
-    public String detallsVehicle(@PathVariable("matricula") String matricula,boolean visualizar, Model model) {
+    public String detallsVehicle(@PathVariable("matricula") String matricula, boolean visualizar, Model model) {
         Vehicle vehicle = vehicleServiceSQL.findByMatricula(matricula).orElse(null);
 
         model.addAttribute("vehicle", vehicle);
@@ -118,30 +138,43 @@ public class AgentDashboardController {
         return "ModificarVehicles";
     }
 
+    /**
+     * Mètode que mostra el formulari per crear un nou vehicle.
+     *
+     * @param model Model de l'objecte per enviar a la vista.
+     * @return Vista per crear un vehicle.
+     */
     @GetMapping("/crear_vehicle")
     public String mostrarFormulariVehicle(Model model) {
         Vehicle vehicle = new Vehicle();
         model.addAttribute("vehicle", vehicle);
-        
         return "crearVehicle";
     }
 
-
-
+    /**
+     * Mètode per crear un vehicle nou a la base de dades.
+     *
+     * @param vehicle Objeto Vehicle amb les dades del vehicle a crear.
+     * @param result Resultat de la validació del formulari.
+     * @param file Fitxer de la imatge del vehicle.
+     * @param model Model de l'objecte per enviar a la vista.
+     * @return Vista de la pàgina següent després de la creació.
+     * @throws IOException Si hi ha un error en guardar la imatge.
+     */
     @PostMapping("/crear")
     public String crearVehicle(@Valid @ModelAttribute Vehicle vehicle, BindingResult result,
                                @RequestParam("imagen") MultipartFile file, Model model) {
         try {
-            // Crear y guardar la imagen
+            // Crear i guardar la imatge
             Imagen image = new Imagen();
             image.setNombre(file.getOriginalFilename());
             image.setType(file.getContentType());
             image.setImageData(file.getBytes());
 
-            // Asociar la imagen con el vehículo
+            // Associar la imatge amb el vehicle
             vehicle.setImage(image);
 
-            // Generar la URL de la imagen
+            // Generar la URL de la imatge
             String base64Image = Base64.getEncoder().encodeToString(image.getImageData());
             String imageUrl = "data:" + image.getType() + ";base64," + base64Image;
             vehicle.setImageUrl(imageUrl);
@@ -153,34 +186,53 @@ public class AgentDashboardController {
                 vehicle.setLocalitat(agent.getLocalitat());
             }
             vehicle.setEstatVehicle(EstatVehicle.INACTIU);
-            if(result.hasErrors()) {
+            if (result.hasErrors()) {
                 return "crearVehicle";
-            }else {
+            } else {
                 vehicleServiceSQL.guardarVehicle(vehicle);
                 return "redirect:/agent/dashboard";
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 
+    /**
+     * Mètode per eliminar un vehicle de la base de dades.
+     *
+     * @param matricula Matricula del vehicle que es vol eliminar.
+     * @return Redirecció a la pàgina principal del panell de l'agent.
+     */
     @GetMapping("/delete/vehicle/{matricula}")
     public String deleteClient(@PathVariable String matricula) {
         vehicleServiceSQL.eliminarVehiclePerId(matricula);
         return "redirect:/agent/dashboard";
     }
 
+    /**
+     * Mètode per editar un vehicle existent.
+     *
+     * @param matricula Matricula del vehicle que es vol editar.
+     * @param model Model de l'objecte per enviar a la vista.
+     * @return Vista per modificar un vehicle.
+     */
     @GetMapping("/edit/vehicle/{matricula}")
     public String editVehicle(@PathVariable String matricula, Model model) {
         Optional<Vehicle> vehicle = vehicleServiceSQL.findByMatricula(matricula);
         if (vehicle.isPresent()) {
             model.addAttribute("vehicle", vehicle.get());
-            model.addAttribute("visualizar", false);;
+            model.addAttribute("visualizar", false);
         }
-        return "ModificarVehicles"; // Nombre del archivo Thymeleaf
+        return "ModificarVehicles"; // Nom del fitxer Thymeleaf
     }
 
+    /**
+     * Mètode per activar un vehicle.
+     *
+     * @param matricula Matricula del vehicle a activar.
+     * @param model Model de l'objecte per enviar a la vista.
+     * @return Redirecció al panell de l'agent.
+     */
     @GetMapping("/activar/vehicle/{matricula}")
     public String activarVehicle(@PathVariable String matricula, Model model) {
         Optional<Vehicle> vehicle = vehicleServiceSQL.findByMatricula(matricula);
@@ -191,6 +243,13 @@ public class AgentDashboardController {
         return "redirect:/agent/dashboard";
     }
 
+    /**
+     * Mètode per desactivar un vehicle.
+     *
+     * @param matricula Matricula del vehicle a desactivar.
+     * @param model Model de l'objecte per enviar a la vista.
+     * @return Redirecció al panell de l'agent.
+     */
     @GetMapping("/desactivar/vehicle/{matricula}")
     public String desactivarVehicle(@PathVariable String matricula, Model model) {
         Optional<Vehicle> vehicle = vehicleServiceSQL.findByMatricula(matricula);
@@ -200,6 +259,14 @@ public class AgentDashboardController {
         }
         return "redirect:/agent/dashboard";
     }
+
+    /**
+     * Mètode per lliurar un vehicle.
+     *
+     * @param matricula Matricula del vehicle a lliurar.
+     * @param model Model de l'objecte per enviar a la vista.
+     * @return Redirecció al panell de l'agent.
+     */
     @GetMapping("/lliurar/vehicle/{matricula}")
     public String lliurarVehicle(@PathVariable String matricula, Model model) {
         Optional<Vehicle> vehicle = vehicleServiceSQL.findByMatricula(matricula);
@@ -210,17 +277,23 @@ public class AgentDashboardController {
         return "redirect:/agent/dashboard";
     }
 
-
+    /**
+     * Mètode per guardar els canvis d'un vehicle després de la seva edició.
+     *
+     * @param vehiculo Vehicle amb les dades actualitzades.
+     * @param matricula Matricula del vehicle que es vol actualitzar.
+     * @param model Model de l'objecte per enviar a la vista.
+     * @return Redirecció al panell de l'agent.
+     */
     @PostMapping("/editVehicle")
     public String guardarCambios(@ModelAttribute Vehicle vehiculo, @RequestParam String matricula, Model model) {
-        // Buscar el vehículo que se está editando por su matrícula enviada en el
-        // formulario
+        // Buscar el vehicle que s'està editant per la seva matrícula enviada al formulari
         Optional<Vehicle> vehiculoExistente = vehicleServiceSQL.findByMatricula(matricula);
 
         if (vehiculoExistente.isPresent()) {
             Vehicle vehiculoACambiar = vehiculoExistente.get();
 
-            // Actualizar los datos del vehículo
+            // Actualitzar les dades del vehicle
             vehiculoACambiar.setMarca(vehiculo.getMarca());
             vehiculoACambiar.setModel(vehiculo.getModel());
             vehiculoACambiar.setPreuDia(vehiculo.getPreuDia());
@@ -236,17 +309,22 @@ public class AgentDashboardController {
             vehiculoACambiar.setEstatVehicle(vehiculo.getEstatVehicle());
             vehiculoACambiar.setAnyVehicle(vehiculo.getAnyVehicle());
             vehiculoACambiar.setKm(vehiculo.getKm());
-            // vehiculoACambiar.setDescVehicle(vehiculo.getDescVehicle());
 
-            // Guardar los cambios
+            // Guardar els canvis
             vehicleServiceSQL.modificarVehicle(vehiculoACambiar);
-            return "redirect:/agent/dashboard"; // Redirigir al panel de administración
+            return "redirect:/agent/dashboard"; // Redirigir al panell d'administració
         } else {
-            model.addAttribute("error", "El vehículo no existe o no es válido.");
-            return "ModificarVehicles"; // Mostrar la página con el error
+            model.addAttribute("error", "El vehicle no existeix o no és vàlid.");
+            return "ModificarVehicles"; // Mostrar la pàgina amb l'error
         }
     }
 
+    /**
+     * Mètode per activar una reserva.
+     *
+     * @param idReserva Identificador de la reserva a activar.
+     * @return Redirecció al panell de l'agent.
+     */
     @GetMapping("/activateUser/{idReserva}")
     public String activateUser(@PathVariable Long idReserva) {
         reservaServiceSQL.activarReserva(idReserva);
--- src/main/java/com/copernic/projecte2_openroad/controllers/ComentariController.java
+++ src/main/java/com/copernic/projecte2_openroad/controllers/ComentariController.java
@@ -10,6 +10,7 @@ import com.copernic.projecte2_openroad.model.mongodb.Comentari;
 import com.copernic.projecte2_openroad.model.mysql.Client;
 import com.copernic.projecte2_openroad.security.UserUtils;
 import com.copernic.projecte2_openroad.service.mongodb.ComentarisServiceMongo;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,21 +23,25 @@ public class ComentariController {
     @Autowired
     private ComentarisServiceMongo comentarisServiceMongo;
 
+    // Endpoint per al formulari de comentari per un vehicle
     @GetMapping("/comentariForm/{matricula}")
-    public String comentariForm(@PathVariable String matricula,  Model model) {
+    public String comentariForm(@PathVariable String matricula, Model model) {
 
+        // Obtenir les dades de l'usuari i passar-les al model
         UserUtils.obtenirDadesUsuariModel(model);
 
         Comentari comentari = new Comentari();
         model.addAttribute("comentari", comentari);
         model.addAttribute("matricula", matricula);
 
-        return "comentariForm";
+        return "comentariForm"; // Retorna la vista del formulari
     }
 
+    // Endpoint per crear un nou comentari
     @PostMapping("/crearComentari")
     public String crearComentari(@ModelAttribute Comentari comentari, @RequestParam String matricula, Model model) {
 
+        // Obtenir les dades de l'usuari per associar-les al comentari
         Object dadesUsuari = UserUtils.obtenirDadesUsuariModel(model);
 
         Client client = new Client();
@@ -44,20 +49,23 @@ public class ComentariController {
             client = (Client) dadesUsuari;
         }
 
+        // Assignar les dades del client al comentari
         comentari.setNomClient(client.getNom());
         comentari.setCognom1Client(client.getCognom1());
         comentari.setNomUsuariClient(client.getNomUsuari());
         comentari.setMatriculaVehicle(matricula);
         LocalDate data = LocalDate.now();
         comentari.setDataComentari(data);
-        comentari.setLike(0);
-        comentari.setDisLike(0);
+        comentari.setLike(0); // Inicialitzar likes
+        comentari.setDisLike(0); // Inicialitzar dislikes
 
+        // Guardar el comentari al servei MongoDB
         comentarisServiceMongo.guardarComentari(comentari);
 
-        return "redirect:/vehicle/" + matricula;
+        return "redirect:/vehicle/" + matricula; // Redirigir al vehicle amb la matrícula corresponent
     }
 
+    // Endpoint per modificar els likes i dislikes d'un comentari
     @PostMapping("/modificarLikes")
     public String modificarLikes(
             @RequestParam String idComentari,
@@ -65,11 +73,16 @@ public class ComentariController {
             @RequestParam int likes,
             @RequestParam int dislikes) {
 
+        // Obtenir el comentari per ID
         Comentari comentari = comentarisServiceMongo.llistarComentariPerId(idComentari);
+
+        // Modificar els likes i dislikes del comentari
         comentari.setLike(likes);
         comentari.setDisLike(dislikes);
+
+        // Actualitzar el comentari al servei MongoDB
         comentarisServiceMongo.modificarComentari(comentari);
 
-        return "redirect:/vehicle/" + matricula;
+        return "redirect:/vehicle/" + matricula; // Redirigir al vehicle amb la matrícula corresponent
     }
 }
